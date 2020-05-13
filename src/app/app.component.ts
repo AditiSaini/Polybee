09981213386cb0f632e75d793633b5707ef66f11
Aditi <aditisaini99@gmail.com>
2020-05-13 17:54:12 +0800

    Hide name on scroll

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,19 +1,13 @@
-import { Component, AfterViewInit, ElementRef } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements AfterViewInit {
-  ngOnInit(): void {}
-
-  constructor(private elementRef: ElementRef) {}
-
-  ngAfterViewInit() {
-    // this.elementRef.nativeElement.ownerDocument.body.style.backgroundColor =
-    //   '#D2B4DE';
-  }
-
+export class AppComponent {
   title = 'Polybee Demo';
 }
+
+// https://medium.com/@adamrackis/making-your-web-app-work-offline-with-service-workers-and-indexeddb-6ce9c538c48
+// https://offering.solutions/blog/articles/2018/11/21/online-and-offline-sync-with-angular-and-indexeddb/
--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -7,15 +7,18 @@ import { DOCUMENT } from '@angular/common';
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent implements OnInit {
+  public scrollFlag: boolean;
+
   constructor(@Inject(DOCUMENT) private document: Document) {}
 
   @HostListener('window:scroll', [])
   onWindowScroll() {
     if (document.body.scrollTop > 0 || document.documentElement.scrollTop > 0) {
-      console.log('Yes');
+      this.scrollFlag = true;
       document.getElementById('jumbo').classList.remove('jumbotron');
       document.getElementById('jumbo').classList.add('jumbotron-shrink');
     } else {
+      this.scrollFlag = false;
       document.getElementById('jumbo').classList.add('jumbotron');
       document.getElementById('jumbo').classList.remove('jumbotron-shrink');
     }
